7a509870de3370b0a832ac8614fc05917a87361b
Manuel83 <manuel.fritsch@gmail.com>
2017-05-17 19:12:34 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,16 @@
+from modules import cbpi
+
+from modules.core.hardware import ActorBase
+
+@cbpi.actor
+class SampleActor(ActorBase):
+    #custom property 
+    prop1 = Property.Text("Property1", True, "1")
+    
+    def on(self, power=0):
+        print "SWITCH OFF %s" % (self.prop1)
+        
+    def off(self, power=0):
+        print "SWITCH OFF"
+
+
